a187048b1a3f1ffe18188de0ac9493b47b18ff93
Harini Rodrigo <77738765+HARINIRODRIGO@users.noreply.github.com>
2024-03-09 14:31:08 +0530

    Create shiftNumbers.js

---
+++ shiftNumbers.js
@@ -0,0 +1,12 @@
+function shiftNum() {
+    var num1 = 3;
+    var num2 = 5;
+
+    var num3 = num2;
+    num2 = num1;
+    num1 = num3
+
+   console.log(name.length)
+    console.log(num2); 
+}
+shiftNum();
