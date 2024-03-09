8a30b61c244ee06bb634de220c865fae06fb55af
Harini Rodrigo <77738765+HARINIRODRIGO@users.noreply.github.com>
2024-03-10 02:54:24 +0530

    Update function.js

--- function.js
+++ function.js
@@ -22,3 +22,17 @@ function evaluateLoveScore(){
 }
 }
 evaluateLoveScore();
+
+function leapYear(year){
+    if(year%4 != 0){
+        console.log("Not a Leap Year");
+    } else if (year%100 != 0 ){
+        console.log("Leap Year");
+    }else if (year%400 == 0){
+        console.log("Leap Year");
+    } else{
+        console.log("Not a Leap Year");
+    }
+}
+
+leapYear(2024);
