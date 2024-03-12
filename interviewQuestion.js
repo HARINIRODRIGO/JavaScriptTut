8cebcec60d81da9c21b21a4004aeb979283381a2
Harini Rodrigo <77738765+HARINIRODRIGO@users.noreply.github.com>
2024-03-12 11:04:02 +0530

    Update interviewQuestion.js

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -12,3 +12,22 @@ function whosPaying(num, data) {
 }
 
 whosPaying("This Document is a document.", "document");
+/*
+Finding the 2nd largest value in an array.
+*/
+
+var array = [25,75,48,63,25,47];
+var data = 0;
+
+for(var i =0; i<array.length; i++){
+   for(var x = i+1; x<array.length; x++){
+      if(array[x]>array[i]){
+         data = array[x];
+         array[x] = array[i];
+         array[i] = data;
+      }
+      
+   }
+}
+
+console.log(array[1]);
